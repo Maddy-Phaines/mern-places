96bb740030617a6dcc761530b4520dd3bebf4581
Maddy <maddyphaines.dev@gmail.com>
2025-11-20 12:08:07 +0000

    feat(client): configure Vite and global styles

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -8,22 +8,62 @@ import Users from "./user/pages/Users";
 import NewPlaces from "./places/pages/NewPlace";
 import MainNavigation from "./shared/components/navigation/MainNavigation";
 import UserPlaces from "./places/pages/UserPlaces";
+import UpdatePlace from "./places/pages/UpdatePlace";
+import Auth from "./user/pages/Auth";
 import NotFound from "./shared/components/NotFound";
+import { AuthContext } from "./shared/context/auth-context";
 
+import { useState, useCallback } from "react";
 const App = () => {
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
+
+  const login = useCallback(() => {
+    setIsLoggedIn(true);
+  }, []);
+
+  const logout = useCallback(() => {
+    setIsLoggedIn(false);
+  }, []);
+
+  let routes;
+  if (isLoggedIn) {
+    routes = (
+      <>
+        <Route path="/" element={<Users />} />
+
+        <Route path="/:userId/places" element={<UserPlaces />} exact />
+        <Route path="/places/new" element={<NewPlaces />} exact />
+
+        <Route path="/places/:placeId" element={<UpdatePlace />} />
+
+        <Route path="*" element={<Navigate to="/" replace />} />
+      </>
+    );
+  } else {
+    routes = (
+      <>
+        <Route path="/" element={<Users />} exact />
+
+        <Route path="/:userId/places" element={<UserPlaces />} exact />
+
+        <Route path="/auth" element={<Auth />} />
+
+        <Route path="*" element={<Navigate to="/auth" replace />} />
+      </>
+    );
+  }
+
   return (
-    <Router>
-      <MainNavigation />
-      <main>
-        <Routes>
-          <Route path="/" element={<Navigate to="/users" />} />
-          <Route path="/users" element={<Users />} />
-          <Route path="/:userId/places" element={<UserPlaces />} />
-          <Route path="/places/new" element={<NewPlaces />} />
-          <Route path="*" element={<NotFound />} />
-        </Routes>
-      </main>
-    </Router>
+    <AuthContext.Provider
+      value={{ isLoggedIn: isLoggedIn, login: login, logout: logout }}
+    >
+      <Router>
+        <MainNavigation />
+        <main>
+          <Routes>{routes}</Routes>
+        </main>
+      </Router>
+    </AuthContext.Provider>
   );
 };
 
